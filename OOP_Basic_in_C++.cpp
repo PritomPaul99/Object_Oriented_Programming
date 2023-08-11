156ed77c6ed4001b8d5709b7929143c5a2b678b3
Pritom Paul <pritompaul1920@gmail.com>
2023-08-11 20:04:07 +0600

    1) added OOP_Basic_in_Python.py, 2) OOP_Basic_in_C++.cpp (but not completed), 3) add Object-&-Classes directory (havn't added anything yet.)

---
+++ OOP_Basic_in_C++.cpp
@@ -0,0 +1,26 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class OOP_Basic_in_C++
+{
+private:
+    /* data */
+public:
+    OOP_Basic_in_C++(/* args */);
+    ~OOP_Basic_in_C++();
+};
+
+OOP_Basic_in_C++::OOP_Basic_in_C++(/* args */)
+{
+}
+
+OOP_Basic_in_C++::~OOP_Basic_in_C++()
+{
+}
+
+
+int main()
+{
+
+}
